1f2c3d33a73d21c9eeeda23195a6224dd29126cf
raniachikhi <raniach0653@gmail.com>
2023-06-07 16:13:39 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,13 +1,6 @@
 #!/usr/bin/python3
-
-# Print numbers from 0 to 99 in a two-digit format with
-# leading zeros if necessary
-# The numbers are separated by commas, except for the last
-# number, 99
-for i in range(100):
-    if i < 10:
-        print("0{}".format(i), end="")
-    elif i == 99:
+for i in range(0, 100):
+    if i == 99:
         print(i)
     else:
-        print(i, end=", ")
+        print("{:0>2d}".format(i), end=", ")
