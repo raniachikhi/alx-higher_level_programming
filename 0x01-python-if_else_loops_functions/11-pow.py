b2966314cef3810e33896d401cf05195ea63c646
raniachikhi <raniach0653@gmail.com>
2023-06-07 16:31:01 -0700

    11-pow.py

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # 11-power.py
 
-def power(a, b):
+def pow(a, b):
     """Return a to the power of b."""
     # Calculate and return the exponentiation of a to the power of b
     return a ** b
