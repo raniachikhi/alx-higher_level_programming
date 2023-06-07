15636e61e0a4258f79b816f68778f15b14763aaf
raniachikhi <raniach0653@gmail.com>
2023-06-07 16:21:07 -0700

    9-print_last_digit.py

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# 10-add.py
+
+def add(a, b):
+    """Return the addition of a and b."""
+    # Add the values of a and b and return the result
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 9-print_last_digit.py
+
+def print_last_digit(number):
+# Calculate the absolute value of the number and
+# find the remainder when divided by 10
+	last_digit = abs(number) % 10
+# Print the last digit without a newline character
+	print(last_digit, end="")
+# Return the last digit
+	return last_digit
