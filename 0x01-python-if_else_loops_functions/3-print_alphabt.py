ea06b758a5eb0fbd8ce4f2d7f3c8772284cb98b1
raniachikhi <raniach0653@gmail.com>
2023-06-07 16:03:59 -0700

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 # Print the lowercase alphabet, excluding 'q' and 'e',
-#without a newline character at the end
+# without a newline character at the end
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")
