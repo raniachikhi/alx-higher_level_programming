9c6ded8ff664ae2257099eb31e06153b9fcc8cb4
raniachikhi <raniach0653@gmail.com>
2023-06-07 16:12:45 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -4,8 +4,10 @@
 # leading zeros if necessary
 # The numbers are separated by commas, except for the last
 # number, 99
-for i in range(0, 100):
-    if i == 99:
+for i in range(100):
+    if i < 10:
+        print("0{}".format(i), end="")
+    elif i == 99:
         print(i)
     else:
-        print("{:0>2d}".format(i), end=", ")
+        print(i, end=", ")
