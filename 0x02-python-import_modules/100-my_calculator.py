5d352013c5a31532831c7932033786f3dbc5f850
raniachikhi <raniach0653@gmail.com>
2023-06-08 16:22:12 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
+    """This script handles basic arithmetic operations."""
     from calculator_1 import add, sub, mul, div 
     import sys
 
@@ -16,4 +16,5 @@ if __name__ == "__main__":
 
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[
+			    2]](a, b)))
