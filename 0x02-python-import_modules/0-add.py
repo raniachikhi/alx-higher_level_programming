38eb766cc39526d29254782b6c3701ba335d2e1b
raniachikhi <raniach0653@gmail.com>
2023-06-08 15:48:27 -0700

    0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-''''The values 1 and 2 are assigned to variables 'a' and 'b', respectively. The sum of 'a' and 'b'
-is then printed using a formatted string.'''
-    from add_0 import add
 
-    a = 1
-    b = 2
-    print("{} + {} = {}".format(a, b, add(a, b)))
+from add_0 import add
+a = 1
+b = 2
+print("{} + {} = {}".format(a, b, add(a, b)))
