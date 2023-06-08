b693317801fec960bc8feb1c8a127fdc8e1c0c1a
raniachikhi <raniach0653@gmail.com>
2023-06-08 15:55:26 -0700

    updated

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,13 +3,6 @@
 if __name__ == "__main__":
     """
     This script calculates and prints the sum of 1 and 2.
-
-    It imports the 'add' function from the 'add_0' module to perform the addition.
-
-    The values 1 and 2 are assigned to variables 'a' and 'b', respectively.
-    The result of the addition is then printed using a formatted string.
-
-    Note: This script assumes the 'add_0' module is available in the same directory.
     """
     from add_0 import add
 
