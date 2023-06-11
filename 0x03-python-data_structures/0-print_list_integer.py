c9bb72402253abd195d4c79e8bcddc39de66704c
raniachikhi <raniach0653@gmail.com>
2023-06-11 10:39:40 -0700

    1-element_at.py

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-#0-print_list_integer.py
-
 def print_list_integer(my_list=[]):
-    """takes a list of int & prints int on separate line"""
 	for n in my_list:
-	print("{:d}".format(n))
+		print("{:d}".format(n))
