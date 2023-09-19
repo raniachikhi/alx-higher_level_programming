99d7bef88e62e1f67259a89376a88987e376e5cb
raniachikhi <raniach0653@gmail.com>
2023-09-19 03:29:47 -0700

    add

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,9 @@
 #!/usr/bin/node
-// a script that searches the second biggest integer in the list of arguments
-if (process.argv.length <= 2) {
+// A script that searches the second biggest integer in the list of arguments.
+
+if (process.argv.length <= 3) {
   console.log(0);
 } else {
-  const numbers = process.argv.slice(2).map(Number);
-
-  if (numbers.some(isNaN)) {
-    console.log("Invalid input. Please provide a list of integers.");
-  } else {
-    const sortedNumbers = numbers.sort((a, b) => b - a);
-
-    console.log(sortedNumbers[1]);
-  }
+  const list = process.argv.sort();
+  console.log(list.reverse()[1]);
 }
