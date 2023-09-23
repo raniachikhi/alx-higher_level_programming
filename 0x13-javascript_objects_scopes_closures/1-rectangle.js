a563c5f6c1e57179791a4936ee18a5edf86da983
raniachikhi:wq <raniach0653@gmail.com>
2023-09-23 07:03:20 -0700

    add

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,14 +1,7 @@
 #!/usr/bin/node
-
 module.exports = class Rectangle {
-  constructor(w, h) {
-    // Check if the arguments are valid, and if not, set width and height to undefined.
-    if (w > 0 && h > 0) {
-      this.width = w;
-      this.height = h;
-    } else {
-      this.width = undefined;
-      this.height = undefined;
-    }
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
   }
 };
