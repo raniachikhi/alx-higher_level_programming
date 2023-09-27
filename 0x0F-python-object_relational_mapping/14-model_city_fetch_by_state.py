74065a4b9788b74f8c83b2aa9324f2d8c3081620
raniachikhi <raniach0653@gmail.com>
2023-09-27 06:16:42 -0700

    add

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,21 +13,21 @@ if __name__ == "__main__":
     # Create a SQLAlchemy engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    
+
     # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
-    
+
     # Create a Session
     session = Session()
-    
+
     # Create database tables based on the models
     Base.metadata.create_all(engine)
 
     # Query and retrieve State and City objects joined by the relationship
     cities = session.query(State, City).join(City).order_by(City.id)
-    
+
     for state, city in cities:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
-    
+
     # Close the session
     session.close()
