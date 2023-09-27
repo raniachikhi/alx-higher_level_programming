4fcb96586d2885b256a15edc89dda474a688738e
raniachikhi <raniach0653@gmail.com>
2023-09-27 05:57:26 -0700

    add

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -24,7 +24,8 @@ if __name__ == "__main__":
     Base.metadata.create_all(engine)
 
     # Query and retrieve all State objects with 'a' in the name
-    states_to_delete = session.query(State).filter(State.name.like('%a%')).all()
+    states_to_delete = session.query(State).filter(
+		    State.name.like('%a%')).all()
 
     # Delete the retrieved State objects
     for state in states_to_delete:
