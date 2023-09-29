4e3a16b600eb9d54b507c974857dfeca59438b70
raniachikhi <raniach0653@gmail.com>
2023-09-28 17:37:53 -0700

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-# Send an HTTP GET request to the provided URL and store the response headers in a variable
-response_headers=$(curl -sI "$1")
-content_length=$(echo "$response_headers" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r')
-
-if [[ "$content_length" =~ ^[0-9]+$ ]]; then
-    echo "$content_length"
-else
-    echo "Unable to determine content length."
-fi
+# takes in a URL, sends a request to that URL, and displays the size of the body of the response
+curl -sI $1 | grep "Content-Length" | cut -d " " -f2
