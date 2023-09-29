fabb5e98f4047ddb5464320b4b9bf9ae33d172ce
raniachikhi <raniach0653@gmail.com>
2023-09-28 17:25:09 -0700

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# This script takes in a URL, sends an HTTP request to that URL, and displays the size of the response body.
-# It uses the 'curl' command to send the request, retrieves the 'Content-Length' header from the response headers,
-# and extracts the size using 'cut'.
-
-# Usage: ./script_name.sh URL
-# Example: ./script_name.sh https://example.com
+# This script takes in a URL, sends an HTTP request to that URL,
+#and displays the size of the response body.
 curl -sI $1 | grep "Content-Length" | cut -d " " -f2
