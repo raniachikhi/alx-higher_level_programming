31414fe2b37a9f7e4922d1e5d98d80a6f2f680cf
raniachikhi <raniach0653@gmail.com>
2023-09-28 17:57:05 -0700

    add

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,9 +9,9 @@ def find_peak(list_of_integers):
     while left < right:
         mid = (left + right) // 2
 
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            left = mid + 1
-        else:
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
             right = mid
+        else:
+            left = mid + 1
 
     return list_of_integers[left]
