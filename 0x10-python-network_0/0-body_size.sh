346beedcb0c948842cfc69266c59eaf145f275de
raniachikhi <raniach0653@gmail.com>
2023-09-28 17:32:00 -0700

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-# This script takes in a URL, sends an HTTP request to that URL,
-#and displays the size of the response body.
+# takes in a URL, sends an HTTP request to it,and displays the size of the response body
 body=$(curl -sL $1)
 size_in_bytes=$(echo -n "$body" | wc -c)
 echo "$size_in_bytes"
