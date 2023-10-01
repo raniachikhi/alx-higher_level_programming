bd3022fe84a779463f291afc36c74218a0b1b89c
raniachikhi <raniach0653@gmail.com>
2023-10-01 15:04:14 -0700

    add

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,11 +1,21 @@
 #!/usr/bin/python3
-"""takes GitHub credentials (username and
-password) and uses the GitHub API to display the user id."""
+"""Lists recent commits from a GitHub repository by a specific user."""
 
 import requests
 from sys import argv
 
 if __name__ == "__main__":
-    auth = (argv[1], argv[2])
-    request = requests.get("https://api.github.com/user", auth=auth)
-    print(request.json().get("id"))
+    repo_name = argv[1]
+    owner_name = argv[2]
+
+    url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
+    else:
+        print(f"Error: {response.status_code}")
