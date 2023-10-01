a1a57c20104514baa452cd42a4a87cfb00136b94
raniachikhi <raniach0653@gmail.com>
2023-10-01 14:46:38 -0700

    add

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,6 +4,7 @@ http://0.0.0.0:5000/search_user with the letter as a parameter."""
 
 import requests
 from sys import argv
+import json
 
 if __name__ == "__main__":
     data = {"q": argv[1] if len(argv) > 1 else ""}
