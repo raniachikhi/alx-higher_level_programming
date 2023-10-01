6459b3f9785388d8dbe7f80ef32a3d86277433d0
raniachikhi <raniach0653@gmail.com>
2023-10-01 14:22:29 -0700

    add

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 """Python script that fetches https://alx-intranet.hbtn.io/status."""
 
-from urllib import request
+from urllib import request, error
 
 if __name__ == "__main__":
-    with request.urlopen("https://alx-intranet.hbtn.io/status") as page:
-        content = page.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    url = "https://alx-intranet.hbtn.io/status"
+    try:
+        with request.urlopen(url) as page:
+            content = page.read()
+            print("Body response:")
+            print("\t- type: {}".format(type(content)))
+            print("\t- content: {}".format(content))
+            print("\t- utf8 content: {}".format(content.decode('utf-8')))
+    except error.URLError as e:
+        print("Error: {}".format(e.reason))
