1330a903e993d2fde0fd4ab0cc5b9ec08ac3e8df
raniachikhi <raniach0653@gmail.com>
2023-11-14 13:04:28 -0800

    add

--- 0x12-javascript-warm_up/100-let_me_const.js
+++ 0x12-javascript-warm_up/100-let_me_const.js
@@ -2,13 +2,3 @@
 // A file that modifies the value of myVar to 333
 
 myVar = 333;
-
-101
-#!/usr/bin/node
-// a function that executes x times a function.
-
-exports.callMeMoby = function (x, theFunction) {
-  for (let i = 0; i < x; i++) {
-    theFunction();
-  }
-};
