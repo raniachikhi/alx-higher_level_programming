eec2caee09cc84695b8d4d0afd60c611fa96e187
raniachikhi <raniach0653@gmail.com>
2023-12-02 16:43:41 -0800

    add

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
+# Script that finds a peak in a list of unsorted integers
 def find_peak(list_of_integers):
     if not list_of_integers:
-        return None  # Return None for an empty list
+        return None
     
     low, high = 0, len(list_of_integers) - 1
 
@@ -10,8 +10,8 @@ def find_peak(list_of_integers):
         mid = (low + high) // 2
 
         if list_of_integers[mid] > list_of_integers[mid + 1]:
-            high = mid  # The peak is likely on the left side
+            high = mid
         else:
-            low = mid + 1  # The peak is likely on the right side or at mid
+            low = mid + 1
 
     return list_of_integers[low]
