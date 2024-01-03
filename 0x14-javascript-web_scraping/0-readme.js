790c6b97c92414269f7bd524a1e8e97938a1d73a
raniachikhi <raniach0653@gmail.com>
2024-01-03 10:35:09 -0800

    add

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
-    console.log(err);
+    console.error(err);
   } else {
     process.stdout.write(data);
   }
